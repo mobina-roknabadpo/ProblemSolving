e77d0974dec0f43cbc770d505d8fd0906672d93b
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-08-30 15:25:53 +0330

    change: change content

--- Solving/algorithm/5diagonalDifference/diagonal.js
+++ Solving/algorithm/5diagonalDifference/diagonal.js
@@ -34,13 +34,8 @@ function diagonalDifference(arr) {
     let a = 0;
     let b = 0;
     for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr[0].length; j++){
-            if(i === j){
-                a += arr[i][j];
-            }if(i === (arr[0].length -1)-j){
-                b+= arr[i][j];
-            }
-        }
+        a += arr[i][i];
+        b += arr[i][arr.length -1 - i];
     }
     return Math.abs(a-b);
 
