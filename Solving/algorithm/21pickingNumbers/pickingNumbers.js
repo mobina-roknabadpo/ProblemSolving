37493eb3a91fba55cbb7fe147cfb9a15748291d3
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-11 10:31:51 +0330

    change: changed picking function

--- Solving/algorithm/21pickingNumbers/pickingNumbers.js
+++ Solving/algorithm/21pickingNumbers/pickingNumbers.js
@@ -1,13 +1,17 @@
-// function pickingNumbers(a) {
-//   // Write your code here
-//   let answer = [];
-//   for (let i = 0; i < a.length; i++) {
-//     for (let j = i + 1; j < a.length; j++) {
-//       if (Math.abs(a[i] - a[j] <= 1)) {
-//         answer.push(a[i]);
-//       }
-//     }
-//   }
-//   console.log(answer);
-// }
-// pickingNumbers([4, 6, 5, 3, 3, 1]);
+function pickingNumbers(a) {
+    // Write your code here
+    const sorted = a.sort((a,b) => a-b);
+    let i=0;
+    let j=1;
+    while(i < sorted.length && j < sorted.length){
+        if(Math.abs(sorted[i] - sorted[j]) > 1){
+            i++;
+            j++;
+        }else{
+            j++;
+        }
+    }
+    return Math.abs(i-j)
+
+}
+pickingNumbers([4, 6, 5, 3, 3, 1]);
